025806a4fb235d41d872f0b1515f085555d0c25c
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-12-01 17:00:57 +0300

    main page: switched from things to events.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,26 +2,40 @@
 
 angular.module('projectsApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    ///////////////////////////////////////////////////////
+    // Declarations
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+    const MODEL_NAME = 'event';
+
+    ///////////////////////////////////////////////////////
+
+
+    //=====================================================
+    // Startup code
+
+    $scope.events = [];
+
+    //=====================================================
+
+
+    //*****************************************************
+    // Implementation
+
+    $http.get('/api/events').success(function(events) {
+      $scope.events = events;
+      socket.syncUpdates(MODEL_NAME, $scope.events);
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
+    $scope.onClearBtnClick = function () {
+      log('onClearBtnClick is NOT implemented yet');
     };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+    $scope.deleteThing = function(event) {
+      $http.delete('/api/events/' + event._id);
     };
 
     $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
+      socket.unsyncUpdates(MODEL_NAME);
     });
+
   });

4cc64e72027b6be8eafd88269a14b120b3093551
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-12-02 06:41:13 +0300

    server, post events: check user input.

--- server/api/event/event.post.js
+++ server/api/event/event.post.js
@@ -5,13 +5,75 @@ var Event = require('./event.model');
 
 module.exports = function(req, res) {
   var reqParams = req.query;
+  var id, timestamp, evtClass, device, duration, snr;
+  var param, paramName;
+
+  // Check id
+  paramName = 'event.id';
+  param = reqParams[paramName];
+  if (!param) {
+    return sendMsgParamMissing(res, paramName);
+  }
+  id = parseInt(param, 10);
+  if (isNaN(id)) {
+    return sendMsgParamInvalid(res, paramName, param);
+  }
+
+  // Check timestamp
+  paramName = 'event.timestamp';
+  param = reqParams[paramName];
+  if (!param) {
+    return sendMsgParamMissing(res, paramName);
+  }
+  timestamp = parseFloat(param);
+  if (isNaN(timestamp)) {
+    return sendMsgParamInvalid(res, paramName, param);
+  }
+  timestamp = new Date(timestamp * 1000);
+
+  // Check class
+  paramName = 'event.class';
+  param = reqParams[paramName];
+  if (!param) {
+    return sendMsgParamMissing(res, paramName);
+  }
+
+  // Check device
+  paramName = 'identity.name';
+  param = reqParams[paramName];
+  if (!param) {
+    return sendMsgParamMissing(res, paramName);
+  }
+
+  // Check duration
+  paramName = 'event.info.duration';
+  param = reqParams[paramName];
+  if (!param) {
+    return sendMsgParamMissing(res, paramName);
+  }
+  duration = parseFloat(param);
+  if (isNaN(duration)) {
+    return sendMsgParamInvalid(res, paramName, param);
+  }
+
+  // Check snr
+  paramName = 'event.info.snr';
+  param = reqParams[paramName];
+  if (!param) {
+    return sendMsgParamMissing(res, paramName);
+  }
+  snr = parseFloat(param);
+  if (isNaN(snr)) {
+    return sendMsgParamInvalid(res, paramName, param);
+  }
+
   var newEvent = {
-    id: parseInt(reqParams['event.id'], 10),
-    timestamp: new Date(parseFloat(reqParams['event.timestamp']) * 1000),
+    id: id,
+    timestamp: timestamp,
     class: reqParams['event.class'],
     device: reqParams['identity.name'],
-    duration: parseFloat(reqParams['event.info.duration']),
-    snr: parseFloat(reqParams['event.info.snr'])
+    duration: duration,
+    snr: snr
   };
 
   Event.create(newEvent, function(err, event) {
@@ -19,3 +81,11 @@ module.exports = function(req, res) {
     return res.status(201).json(event);
   });
 };
+
+function sendMsgParamMissing(res, paramName) {
+  return res.status(403).send(paramName + " parameter is missing");
+}
+
+function sendMsgParamInvalid(res, paramName, param) {
+  return res.status(403).send("Invalid " + paramName + " '" + param + "'");
+}
